7e8e0417f3264dacec5885443045ed2cc477afb7
jhw3355 <jhwang0324@gmail.com>
2024-08-28 11:57:37 +0800

    switched to canvas grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,45 @@
 import { useState, useEffect, useRef } from "react";
 import { random, floor } from "lodash";
-import { Grid, Cell } from "./StyledComps";
 import { SnakeArrayType, SnakeDirType, coords } from "./types";
 
-const gridSideLength = 35
-;
+const gridSideLength = 35;
+
 const cellSideLength = 10;
 
-const initialSnakeLength = 5;
-const initialSnake: SnakeArrayType = getInitialSnake();
+const initialSnakeLength = 3;
 
-function getInitialSnake() {
+const initialSnake: SnakeArrayType = (() => {
    const arr: SnakeArrayType = [];
    const y = floor(gridSideLength / 2);
-   for (let x = initialSnakeLength + 1; x > 1; x--) {
+   for (let x = initialSnakeLength; x > 0; x--) {
       arr.push([x, y]);
    };
    return arr;
-}
-
+})();
 
 const gameTick = 120;
 
 export default function App() {
 
-   const [snake, setSnake] = useState<SnakeArrayType>(initialSnake);
-
+   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+   const snake = useRef<SnakeArrayType>(initialSnake);
    const food = useRef<coords>([NaN, NaN]);
-
    const snakeDir = useRef<SnakeDirType>('RIGHT');
 
    const [isGameRunning, setIsGameRunning] = useState<boolean>(false);
    const [showGameOverMsg, setShowGameOverMsg] = useState<boolean>(false);
-
    const [score, setScore] = useState<number>(0);
+
+   useEffect(() => {drawCanvas()}, []);
 
    useEffect(() => {
       // GAME LOOP
-
       let gameInterval: ReturnType<typeof setInterval> | undefined;
-  
+
       if (isGameRunning) {
          gameInterval = setInterval(() => {
             tryToAdvanceSnake();
+            drawCanvas();
          }, gameTick);
       }
   
@@ -70,99 +67,73 @@ export default function App() {
                break;
          }
       };
-
       window.addEventListener("keydown", handleKeyPress);
       return () => window.removeEventListener("keydown", handleKeyPress);
    }, []);
 
-   function generateGrid() {
-      const grid = [];
-      for (let y = gridSideLength; y > 0; y--) {
-         for (let x = 1; x <= gridSideLength; x++) {
-   
-            const isPartOfSnake = snake.some(([snake_x, snake_y]) => snake_x === x && snake_y === y);
-
-            const [food_x, food_y] = food.current;
-            const isFood = (food_x === x && food_y === y);
-   
-            grid.push(
-               <Cell
-                  key={`${x}-${y}`}
-                  $snake={isPartOfSnake}
-                  $food={isFood}
-               ></Cell>
-            );
-         };
-      }
-      return grid;
-   };
-
    function tryToAdvanceSnake() {
-      // Changes 'snake' state.
-      setSnake(previousSnake => {
-         console.log("ADVANCING", snakeDir.current);
-         let newSnake: SnakeArrayType;
-         let [snakeHead_x, snakeHead_y] = previousSnake[0];
+      const previousSnake = snake.current;
+      console.log("ADVANCING", snakeDir.current);
+      let newSnake: SnakeArrayType;
+      let [snakeHead_x, snakeHead_y] = previousSnake[0];
 
-         switch (snakeDir.current) {
-            case 'UP':
-               snakeHead_y++;
-               break;
-            case 'DOWN':
-               snakeHead_y--;
-               break;
-            case 'LEFT':
-               snakeHead_x--;
-               break;
-            case 'RIGHT':
-               snakeHead_x++;
-               break;
-         };
+      switch (snakeDir.current) {
+         case 'UP':
+            snakeHead_y--;
+            break;
+         case 'DOWN':
+            snakeHead_y++;
+            break;
+         case 'LEFT':
+            snakeHead_x--;
+            break;
+         case 'RIGHT':
+            snakeHead_x++;
+            break;
+      };
 
-         const cellToAdvanceInto: coords = [snakeHead_x, snakeHead_y];
-         const snakeWithoutTailCell = previousSnake.slice(0, -1);
+      const cellToAdvanceInto: coords = [snakeHead_x, snakeHead_y];
+      const snakeWithoutTailCell = previousSnake.slice(0, -1);
 
-         const [food_x, food_y] = food.current;
-         if (snakeHead_x === food_x && snakeHead_y === food_y) {
-            // Snake eating food
-            // Length must increase
-            // Another food placed elsewhere
-            newSnake = [cellToAdvanceInto, ...previousSnake];
-            food.current = generateNewFood(newSnake);
-            setScore(prev => prev + 1);
-            console.log("FOOD EATEN: SCORE INCREASED");
-            return newSnake;
+      const [food_x, food_y] = food.current;
+      if (snakeHead_x === food_x && snakeHead_y === food_y) {
+         // Snake eating food
+         // Length must increase
+         // Another food placed elsewhere
+         newSnake = [cellToAdvanceInto, ...previousSnake];
+         food.current = generateNewFood(newSnake);
+         setScore(prev => prev + 1);
+         console.log("FOOD EATEN: SCORE INCREASED");
+         snake.current = newSnake;
+      } else {
+         // Check if snake is advancing out of the grid:
+         const isOutOfBounds = (
+            snakeHead_x < 0 || snakeHead_x === gridSideLength
+            || snakeHead_y < 0 || snakeHead_y === gridSideLength
+         );
+         // Check if snake is advancing into itself 
+         // (tail doenst count since it moves out of the way anyway):
+         const isCollidingWithSelf = (
+            snakeWithoutTailCell.some(([x, y]) => x === snakeHead_x && y === snakeHead_y)
+         );
+         if (isOutOfBounds || isCollidingWithSelf) {
+            // Game ends
+            setIsGameRunning(false);
+            setShowGameOverMsg(true);
+            console.log("GAME OVER!");
          } else {
-            // Check if snake is advancing out of the grid:
-            const isOutOfBounds = (
-               snakeHead_x < 1 || snakeHead_x > gridSideLength
-               || snakeHead_y < 1 || snakeHead_y > gridSideLength
-            );
-            // Check if snake is advancing into itself 
-            // (tail doenst count since it moves out of the way anyway):
-            const isCollidingWithSelf = (
-               snakeWithoutTailCell.some(([x, y]) => x === snakeHead_x && y === snakeHead_y)
-            );
-            if (isOutOfBounds || isCollidingWithSelf) {
-               // Game ends
-               setIsGameRunning(false);
-               setShowGameOverMsg(true);
-               console.log("GAME OVER!");
-               return previousSnake;
-            } else {
-               // Game continues
-               newSnake = [cellToAdvanceInto, ...snakeWithoutTailCell]
-               return newSnake;
-            };
+            // Game continues
+            newSnake = [cellToAdvanceInto, ...snakeWithoutTailCell];
+            snake.current = newSnake;
          };
-      });
+      };
    };
 
    function generateNewFood(snakeArray: SnakeArrayType): coords {
       let food_x: number, food_y: number;
       do {
-         food_x = random(1, gridSideLength);
-         food_y = random(1, gridSideLength);
+         food_x = random(0, gridSideLength-1);
+         food_y = random(0, gridSideLength-1);
       } while (snakeArray.some(([x, y]) => x === food_x && y === food_y));
       return [food_x, food_y];
    };
@@ -170,22 +141,67 @@ export default function App() {
    function startNewGame() {
       setIsGameRunning(true);
       setShowGameOverMsg(false);
-      setSnake(initialSnake);
+      snake.current = initialSnake;
       snakeDir.current = "RIGHT";
       setScore(0);
       food.current = generateNewFood(initialSnake);
    };
 
+   function drawCanvas() {
+      // console.log("DRAWING CANVAS");
+      const canvas = canvasRef.current;
+      if (!canvas) return;
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+      // DRAW GRID
+      ctx.strokeStyle = "lightgray";
+      ctx.lineWidth = 0.5;
+      ctx.beginPath();
+      for (let i = 0; i <= gridSideLength; i++) {
+         const pos = i * cellSideLength;
+         ctx.moveTo(pos, 0);
+         ctx.lineTo(pos, canvas.height);
+         ctx.moveTo(0, pos);
+         ctx.lineTo(canvas.width, pos);
+      };
+      ctx.stroke();
+      
+      ctx.fillStyle = "black";
+
+      snake.current.forEach(([snakeCell_x, snakeCell_y]) => {
+         ctx.fillRect(
+            snakeCell_x * cellSideLength,
+            snakeCell_y * cellSideLength,
+            cellSideLength,
+            cellSideLength
+         );
+      });
+
+      ctx.fillStyle = "red";
+
+      const [food_x, food_y] = food.current;
+      ctx.fillRect(
+         food_x * cellSideLength,
+         food_y * cellSideLength,
+         cellSideLength,
+         cellSideLength
+      );
+   };
+
    return (
       <>
-         <Grid
-            $gridSideLength={gridSideLength}
-            $cellSideLength={cellSideLength}
-         >
-            {generateGrid()}
-         </Grid>
+         <canvas
+            width={gridSideLength*cellSideLength}
+            height={gridSideLength*cellSideLength}
+            style={{border: "1px solid black"}}
+            ref={canvasRef}
+         ></canvas>
 
          <div>
+
             <div
                style={{fontSize: "15px", marginTop: "10px"}}
             >
@@ -198,14 +214,21 @@ export default function App() {
                &nbsp;&nbsp;
                Score: {score}
             </div>
+
+            <br/>
+
             {showGameOverMsg && 
-               <h3 style={{color: "red"}}><strong>GAME OVER</strong></h3>
+               <h3 style={{color: "red", margin: "0"}}><strong>GAME OVER</strong></h3>
             }
+
+            <br/>
+
          </div>
-         <br/>
+         
          <div>Food is at: [{food.current.join(', ')}]</div>
-         <div>Snake length: {snake.length}</div>
-         <div>Use WASD to change direction</div>
+         <div>Snake length: {snake.current.length}</div>
+         <div>Snake is moving: {snakeDir.current}</div>
+         <div>(Use WASD to change direction)</div>
       </>
    );
 }
--- src/StyledComps.tsx
+++ src/StyledComps.tsx
@@ -1,28 +1,28 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-type GridProps = { $gridSideLength: number; $cellSideLength: number };
-type CellProps = { $snake: boolean; $food: boolean };
+// type GridProps = { $gridSideLength: number; $cellSideLength: number };
+// type CellProps = { $snake: boolean; $food: boolean };
 
-export const Grid = styled.div<GridProps>`
-   --gridTotalWidthPx: ${props =>
-      props.$gridSideLength * props.$cellSideLength}px;
-   --gridSideLength: ${props => props.$gridSideLength};
+// export const Grid = styled.div<GridProps>`
+//    --gridTotalWidthPx: ${props =>
+//       props.$gridSideLength * props.$cellSideLength}px;
+//    --gridSideLength: ${props => props.$gridSideLength};
 
-   width: var(--gridTotalWidthPx);
-   height: var(--gridTotalWidthPx);
-   border: 1px solid #e7e7e7;
+//    width: var(--gridTotalWidthPx);
+//    height: var(--gridTotalWidthPx);
+//    border: 1px solid #e7e7e7;
 
-   display: grid;
-   grid-template-columns: repeat(var(--gridSideLength), 1fr);
-   grid-template-rows: repeat(var(--gridSideLength), 1fr);
-`;
+//    display: grid;
+//    grid-template-columns: repeat(var(--gridSideLength), 1fr);
+//    grid-template-rows: repeat(var(--gridSideLength), 1fr);
+// `;
 
-export const Cell = styled.div<CellProps>`
-   border: 1px solid #e7e7e7;
+// export const Cell = styled.div<CellProps>`
+//    border: 1px solid #e7e7e7;
 
-   background-color: ${props => {
-      if (props.$snake) return "rgb(0, 0, 0)";
-      if (props.$food) return "rgb(163, 47, 47)";
-      return "white";
-   }};
-`;
+//    background-color: ${props => {
+//       if (props.$snake) return "rgb(0, 0, 0)";
+//       if (props.$food) return "rgb(163, 47, 47)";
+//       return "white";
+//    }};
+// `;
